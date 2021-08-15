2e32a3d66eb31ee1aae2fa805b5dfb59db886090
shaaz-pragmakw <shaaz@pragmakw.com>
2021-08-15 22:09:27 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Shaaz Khan on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
